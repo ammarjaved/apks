ac999e5c7441b6006d01afca5018d25171a9fc8f
arehman5110 <a.rehman5110@gmail.com>
2024-03-07 13:13:54 +0500

    update

--- app/Http/Controllers/web/Patrolling/PatrollingLKSController.php
+++ app/Http/Controllers/web/Patrolling/PatrollingLKSController.php
@@ -63,7 +63,7 @@ class PatrollingLKSController extends Controller
 
 
         //   $datas = $result->select('id','time' ,'vist_date' , 'wp_name','cycle','reading_end','reading_start','image_reading_start','image_reading_end' ,DB::raw('st_x(st_centroid(ST_Envelope(geom))) as x'),DB::raw('st_y(st_centroid(ST_Envelope(geom))) as y'))->get();
-          return view('patrolling.pdf-template', ['data'=>$datas,'ba'=>$req->ba , 'from_date' =>$from_date , 'to_date'=>$to_date]);
+        //   return view('patrolling.pdf-template', ['data'=>$datas,'ba'=>$req->ba , 'from_date' =>$from_date , 'to_date'=>$to_date]);
         $html = View::make('patrolling.pdf-template', ['data'=>$datas,'ba'=>$req->ba , 'from_date' =>$from_date , 'to_date'=>$to_date])->render();
 
         $pdf = SnappyPdf::loadHTML($html);
